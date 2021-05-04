472119bbe6a0ccd7c997d396b8a5e1de3de09fdd
Sono Willbrand <sono00@web.de>
2021-05-04 08:54:02 +0200

    Setup extension

---
+++ popup/popupTrader.js
@@ -0,0 +1,28 @@
+/**
+ * CSS to hide unnecessary elemtents on the website. 
+ * Only show the purchase button
+ * 
+ */
+const hidePage = `body > :not(.beastify-image) {
+    display: none;
+  }`;
+
+/**
+ * There was an error executing the script.
+ * Display the popup's error message, and hide the normal UI.
+ */
+function reportExecuteScriptError(error) {
+  document.querySelector("#popup-content").classList.add("hidden");
+  document.querySelector("#error-content").classList.remove("hidden");
+  console.error(`Failed to execute beastify content script: ${error.message}`);
+}
+
+/**
+ * When the popup loads, inject a content script into the active tab,
+ * and add a click handler.
+ * If we couldn't inject the script, handle the error.
+ */
+browser.tabs
+  .executeScript({ file: "/content_scripts/beastify.js" })
+  .then(listenForClicks)
+  .catch(reportExecuteScriptError);
