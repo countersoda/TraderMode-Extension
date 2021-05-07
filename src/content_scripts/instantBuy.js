45af8aa2b22254e150bd98b4a4811ae586c8c8bf
Sono Willbrand <sono00@web.de>
2021-05-07 11:16:42 +0200

    Checking connection

--- src/content_scripts/instantBuy.js
+++ src/content_scripts/instantBuy.js
@@ -1,64 +1,63 @@
 console.log("Enter content scripts");
 
-var counter = 0;
-var portFromBS;
+// var counter = 0;
 
-browser.runtime.onConnect.addListener(connected)
+var myPort = browser.runtime.connect({name:"port-from-cs"});
+myPort.postMessage({greeting: "hello from content script"});
 
-function connected(p) {
-  console.log('Connecting...')
-  portFromBS = p;
-  portFromBS.postMessage({greeting: "hi there content script!"});
-  portFromBS.onMessage.addListener(function(m) {
-    console.log("In background script, received message from content script")
-    console.log(m.greeting);
-  });
-}
+myPort.onMessage.addListener(function(m) {
+  console.log("In content script, received message from background script: ");
+  console.log(m.greeting);
+});
+
+document.body.addEventListener("click", function() {
+  myPort.postMessage({greeting: "they clicked the page!"});
+});
 
 // init().catch(console.log);
 
-function init() {
-  browser.runtime.onMessage.addListener((request) => {
-    console.log("Request: " + request.command);
-    if (request.command === "init") {
-      setTimeout(() => clickBuyBtn(), 1000);
-    } else if (request.command === "buy") {
-      clickBuyBtn();
-    }
-    return true;
-  });
-}
+// function init() {
+//   browser.runtime.onMessage.addListener((request) => {
+//     console.log("Request: " + request.command);
+//     if (request.command === "init") {
+//       setTimeout(() => clickBuyBtn(), 1000);
+//     } else if (request.command === "buy") {
+//       clickBuyBtn();
+//     }
+//     return true;
+//   });
+// }
 
-async function clickBuyBtn() {
-  if (counter > 10) return;
-  counter++;
-  var buyBtn = await window.document.getElementsByClassName(
-    "ButtonBase__StyledButton-sc-1qgxh2e-0 gjCpfL Button__StyledButton-ig3kkl-1 fXrqGh"
-  );
-  if (buyBtn[0] !== undefined) {
-    buyBtn[0].click();
-    setTimeout(function () {
-      clickConfirmBtn();
-    }, 0.0001 * 1000);
-  } else {
-    setTimeout(function () {
-      clickBuyBtn();
-    }, 500);
+// async function clickBuyBtn() {
+//   if (counter > 10) return;
+//   counter++;
+//   var buyBtn = await window.document.getElementsByClassName(
+//     "ButtonBase__StyledButton-sc-1qgxh2e-0 gjCpfL Button__StyledButton-ig3kkl-1 fXrqGh"
+//   );
+//   if (buyBtn[0] !== undefined) {
+//     buyBtn[0].click();
+//     setTimeout(function () {
+//       clickConfirmBtn();
+//     }, 0.0001 * 1000);
+//   } else {
+//     setTimeout(function () {
+//       clickBuyBtn();
+//     }, 500);
 
-    console.log("Buy button is not available\nTrying again...");
-  }
-}
+//     console.log("Buy button is not available\nTrying again...");
+//   }
+// }
 
-async function clickConfirmBtn() {
-  var confirmBtn = await window.document.getElementsByClassName(
-    "ButtonBase__StyledButton-sc-1qgxh2e-0 gjCpfL Button__StyledButton-ig3kkl-1 GJBBL"
-  );
-  if (confirmBtn[0] !== undefined) {
-    confirmBtn[0].click();
-  } else {
-    setTimeout(function () {
-      clickConfirmBtn();
-    }, 0.0001 * 1000);
-    console.log("Confirm button is not availableTrying again...");
-  }
-}
+// async function clickConfirmBtn() {
+//   var confirmBtn = await window.document.getElementsByClassName(
+//     "ButtonBase__StyledButton-sc-1qgxh2e-0 gjCpfL Button__StyledButton-ig3kkl-1 GJBBL"
+//   );
+//   if (confirmBtn[0] !== undefined) {
+//     confirmBtn[0].click();
+//   } else {
+//     setTimeout(function () {
+//       clickConfirmBtn();
+//     }, 0.0001 * 1000);
+//     console.log("Confirm button is not availableTrying again...");
+//   }
+// }
--- src/popup/popupTrader.js
+++ src/popup/popupTrader.js
@@ -1,70 +1,67 @@
-var isOn;
-myStorage = window.localStorage;
+// var isOn;
+// myStorage = window.localStorage;
 
-async function init() {
-  var value = myStorage.getItem("value");
-  bVal = value === "true" ? true : false;
-  document.querySelector("#traderCheckBox").checked = bVal;
+// async function init() {
+//   var value = myStorage.getItem("value");
+//   bVal = value === "true" ? true : false;
+//   document.querySelector("#traderCheckBox").checked = bVal;
 
-  if (value === undefined) {
-    setValue(false);
-  } else {
-    setValue(bVal);
-    if (bVal) {
-      sendingMessage();
-    }
-  }
-}
-
-async function setValue(value) {
-  isOn = value;
-  myStorage.setItem("value", value);
-}
-
-function sendingMessage() {
-  var tabs = browser.tabs.query({
-    currentWindow: true,
-  });
-  tabs.then(connectToTab);
-}
-
-function connectToTab(tabs) {
-  let availablePort;
-  for (let i = 0; i < tabs.length; i++) {
-    var port = browser.tabs.connect(tabs[i].id, {
-      name: "tabs-connect-example",
-    });
-    console.log(port)
-    if (port !== undefined) availablePort = port;
-  }
-  availablePort.postMessage({ greeting: "Hi from background script" });
-}
-
-// function traderOn() {
-//   if (isOn !== undefined && isOn) {
-//     browser.tabs
-//       .executeScript({ file: "/src/content_scripts/instantBuy.js" })
-//       .catch(reportExecuteScriptError);
+//   if (value === undefined) {
+//     setValue(false);
+//   } else {
+//     setValue(bVal);
+//     if (bVal) {
+//     }
 //   }
 // }
 
-/**
- * There was an error executing the script.
- * Display the popup's error message, and hide the normal UI.
- */
-function reportExecuteScriptError(error) {
-  document.querySelector("#error-content").classList.remove("hidden");
-  console.log("Failed to execute traderMode content script: ", error);
+// async function setValue(value) {
+//   isOn = value;
+//   myStorage.setItem("value", value);
+// }
+
+var portFromCS;
+
+function connected(p) {
+  portFromCS = p;
+  portFromCS.postMessage({greeting: "hi there content script!"});
+  portFromCS.onMessage.addListener(function(m) {
+    console.log("In background script, received message from content script")
+    console.log(m.greeting);
+  });
 }
 
-document.getElementById("traderToggle").onchange = function () {
-  setValue(!isOn);
-};
+browser.runtime.onConnect.addListener(connected);
 
-document.getElementById("traderButton").onclick = function () {
-  browser.tabs
-    .executeScript({ file: "/src/content_scripts/instantBuy.js" })
-    .catch(console.log);
-};
+browser.browserAction.onClicked.addListener(function() {
+  portFromCS.postMessage({greeting: "they clicked the button!"});
+});
 
-init().catch((e) => console.log("ERROR:\n" + e));
+// // function traderOn() {
+// //   if (isOn !== undefined && isOn) {
+// //     browser.tabs
+// //       .executeScript({ file: "/src/content_scripts/instantBuy.js" })
+// //       .catch(reportExecuteScriptError);
+// //   }
+// // }
+
+// /**
+//  * There was an error executing the script.
+//  * Display the popup's error message, and hide the normal UI.
+//  */
+// function reportExecuteScriptError(error) {
+//   document.querySelector("#error-content").classList.remove("hidden");
+//   console.log("Failed to execute traderMode content script: ", error);
+// }
+
+// document.getElementById("traderToggle").onchange = function () {
+//   setValue(!isOn);
+// };
+
+// document.getElementById("traderButton").onclick = function () {
+//   browser.tabs
+//     .executeScript({ file: "/src/content_scripts/instantBuy.js" })
+//     .catch(console.log);
+// };
+
+// init().catch((e) => console.log("ERROR:\n" + e));
