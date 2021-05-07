fe4c2f7616635f0995e0c3886979ac86d01372b7
Sono Willbrand <sono00@web.de>
2021-05-07 11:00:39 +0200

    Figuring out connection between content and background script

--- src/content_scripts/instantBuy.js
+++ src/content_scripts/instantBuy.js
@@ -1,13 +1,36 @@
-console.log("Enter background scripts");
-// run().error(console.log);
-var counter = 0;
+console.log("Enter content scripts");
 
-setTimeout(function () {
-  clickBuyBtn();
-}, 1500);
+var counter = 0;
+var portFromBS;
+
+browser.runtime.onConnect.addListener(connected)
+
+function connected(p) {
+  console.log('Connecting...')
+  portFromBS = p;
+  portFromBS.postMessage({greeting: "hi there content script!"});
+  portFromBS.onMessage.addListener(function(m) {
+    console.log("In background script, received message from content script")
+    console.log(m.greeting);
+  });
+}
+
+// init().catch(console.log);
+
+function init() {
+  browser.runtime.onMessage.addListener((request) => {
+    console.log("Request: " + request.command);
+    if (request.command === "init") {
+      setTimeout(() => clickBuyBtn(), 1000);
+    } else if (request.command === "buy") {
+      clickBuyBtn();
+    }
+    return true;
+  });
+}
 
 async function clickBuyBtn() {
-  if (counter > 100) return;
+  if (counter > 10) return;
   counter++;
   var buyBtn = await window.document.getElementsByClassName(
     "ButtonBase__StyledButton-sc-1qgxh2e-0 gjCpfL Button__StyledButton-ig3kkl-1 fXrqGh"
--- src/popup/popupTrader.js
+++ src/popup/popupTrader.js
@@ -1,30 +1,58 @@
 var isOn;
 myStorage = window.localStorage;
 
+async function init() {
+  var value = myStorage.getItem("value");
+  bVal = value === "true" ? true : false;
+  document.querySelector("#traderCheckBox").checked = bVal;
+
+  if (value === undefined) {
+    setValue(false);
+  } else {
+    setValue(bVal);
+    if (bVal) {
+      sendingMessage();
+    }
+  }
+}
+
 async function setValue(value) {
   isOn = value;
   myStorage.setItem("value", value);
 }
 
-async function init() {
-  var value = myStorage.getItem("value");
-  bVal = value === "true" ? true : false;
-  document.querySelector("#traderCheckBox").checked = bVal;
-  if (value === undefined) {
-    isOn = false;
-    setValue(isOn);
-  }
-  setValue(bVal);
+function sendingMessage() {
+  var tabs = browser.tabs.query({
+    currentWindow: true,
+  });
+  tabs.then(connectToTab);
 }
 
-init().catch((e) => console.log("ERROR:\n" + e));
+function connectToTab(tabs) {
+  let availablePort;
+  for (let i = 0; i < tabs.length; i++) {
+    var port = browser.tabs.connect(tabs[i].id, {
+      name: "tabs-connect-example",
+    });
+    console.log(port)
+    if (port !== undefined) availablePort = port;
+  }
+  availablePort.postMessage({ greeting: "Hi from background script" });
+}
+
+// function traderOn() {
+//   if (isOn !== undefined && isOn) {
+//     browser.tabs
+//       .executeScript({ file: "/src/content_scripts/instantBuy.js" })
+//       .catch(reportExecuteScriptError);
+//   }
+// }
 
 /**
  * There was an error executing the script.
  * Display the popup's error message, and hide the normal UI.
  */
 function reportExecuteScriptError(error) {
-  // document.querySelector("#popup-content").classList.add("hidden");
   document.querySelector("#error-content").classList.remove("hidden");
   console.log("Failed to execute traderMode content script: ", error);
 }
@@ -33,16 +61,10 @@ document.getElementById("traderToggle").onchange = function () {
   setValue(!isOn);
 };
 
-async function traderOn() {
-  if (isOn !== undefined && isOn) {
-    browser.tabs
-      .executeScript({ file: "/src/content_scripts/instantBuy.js" })
-      .catch(reportExecuteScriptError);
-  }
-}
-
 document.getElementById("traderButton").onclick = function () {
-  traderOn().catch(console.log);
+  browser.tabs
+    .executeScript({ file: "/src/content_scripts/instantBuy.js" })
+    .catch(console.log);
 };
 
-traderOn().catch(console.log);
+init().catch((e) => console.log("ERROR:\n" + e));
