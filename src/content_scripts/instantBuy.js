e252097d5f7bcaf99ec301e9c151d8df10a33e25
Sono Willbrand <sono00@web.de>
2021-05-10 13:31:40 +0200

    Debug dapper buy button

--- src/content_scripts/instantBuy.js
+++ src/content_scripts/instantBuy.js
@@ -1,33 +1,39 @@
 console.log("Enter content scripts");
 var counter = 0;
-
+console.log("Counter" + counter);
 myStorage = browser.storage.local;
+
 myStorage.get("buttonOn").then((value) => {
   if (value.buttonOn.value) {
     setTimeout(() => clickBuyBtn(), 800);
   }
 });
+
 myStorage.get("rageOn").then((value) => {
+  console.log("Rage: " + value.rageOn.value);
   if (value.rageOn.value) {
-    setTimeout(() => rageClickBuyBtn(), 800);
+    setTimeout(() => clickBuyBtn(), 800);
+    setTimeout(() => dapperBuyBtn(), 2000);
   }
 });
+
 instant().catch(console.log);
 
 function instant() {
   browser.runtime.onMessage.addListener((request) => {
     console.log("Request: " + request.command);
+    console.log("Equal rage = " + request.command === "rage");
     if (request.command === "buy") {
       clickBuyBtn();
-    } else if ((request.command = "rage")) {
-      setTimeout(() => rageClickBuyBtn(), 800);
+    } else if (request.command === "rage") {
+      setTimeout(() => dapperBuyBtn(), 800);
     }
     return true;
   });
 }
 
 async function clickBuyBtn() {
-  if (counter++ > 10) return;
+  if (counter++ > 50) return;
   var buyBtn = await window.document.getElementsByClassName(
     "ButtonBase__StyledButton-sc-1qgxh2e-0 gjCpfL Button__StyledButton-ig3kkl-1 fXrqGh"
   );
@@ -49,55 +55,25 @@ async function clickConfirmBtn() {
   );
   if (confirmBtn[0] !== undefined) {
     confirmBtn[0].click();
+    confirmClicked = true;
+    counter = 0;
   } else {
     setTimeout(clickConfirmBtn, 0.0001 * 1000);
     console.log("Confirm button is not available\nTrying again...");
   }
 }
 
-/**
- * Those functions work similar to the normal functions.
- * This workflow below works for the rage mode
- */
-
-async function rageClickBuyBtn() {
-  var buyBtn = await window.document.getElementsByClassName(
-    "ButtonBase__StyledButton-sc-1qgxh2e-0 gjCpfL Button__StyledButton-ig3kkl-1 fXrqGh"
-  );
-  if (buyBtn[0] !== undefined) {
-    buyBtn[0].click();
-    rageClickConfirmBtn();
-  } else {
-    setTimeout(function () {
-      rageClickBuyBtn();
-    }, 0.0001 * 1000);
-
-    console.log("Buy button is not available\nTrying again...");
-  }
-}
-
-async function rageClickConfirmBtn() {
-  var confirmBtn = await window.document.getElementsByClassName(
-    "ButtonBase__StyledButton-sc-1qgxh2e-0 gjCpfL Button__StyledButton-ig3kkl-1 GJBBL"
-  );
-  if (confirmBtn[0] !== undefined) {
-    confirmBtn[0].click();
-    setTimeout(rageDapperBuyBtn, 8 * 1000);
-  } else {
-    setTimeout(rageClickConfirmBtn, 0.0001 * 1000);
-    console.log("Confirm button is not available\nTrying again...");
-  }
-}
-
-async function rageDapperBuyBtn() {
-  console.log('Enter Dapper Function')
+async function dapperBuyBtn() {
+  console.log("Enter Dapper Function");
+  if (counter++ > 50) return;
+  
   var buyBtn = await window.document.getElementsByClassName("css-ftq8xn");
   if (buyBtn[0] !== undefined) {
-    console.log('Dapper button found')
+    console.log("Dapper button found");
     buyBtn[0].click();
   } else {
     setTimeout(function () {
-      rageDapperBuyBtn();
+      dapperBuyBtn();
     }, 0.0001 * 1000);
     console.log("Dapper button is not available\nTrying again...");
   }
