74e746173e488a9d5165e78ebb45bec905eb22e7
Sono Willbrand <sono00@web.de>
2021-05-11 15:55:16 +0200

    Clean up logs

--- src/content_scripts/wax-adapter.js
+++ src/content_scripts/wax-adapter.js
@@ -1,31 +1,5 @@
 const fetch = require("node-fetch");
 
-// const body = {
-//   block_num_or_id: "118561703",
-// };
-//
-// const communityURL = "https://wax.dfuse.eosnation.io/v1/chain/get_block";
-//
-// fetch(communityURL, {
-//   method: "post",
-//   body: JSON.stringify(body),
-// })
-//   .then((val) => val.json())
-//   .then((val) => printTransactions(val.transactions));
-
-// function printTransactions(transactions) {
-//   for (let i = 0; i < transactions.length; i++) {
-//     // console.log(transactions[i].trx);
-//     let trx = transactions[i].trx;
-//     if (trx.transaction !== undefined) {
-//       let actions = trx.transaction.actions;
-//       for (let j = 0; j < actions.length; j++) {
-//         // console.log(actions[j].data);
-//       }
-//     }
-//   }
-// }
-
 const waxioURL = "https://wax.greymass.com/v1/history/get_actions";
 
 const accountName = {
