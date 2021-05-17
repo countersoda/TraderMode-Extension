1a5e086f97b8684365c79e53c3ac097868a2285b
Sono Willbrand <sono00@web.de>
2021-05-17 10:05:28 +0200

    fix memo bug

--- src/popup/main.js
+++ src/popup/main.js
@@ -46,11 +46,9 @@ var memo = {
   seed: "",
 };
 
-async function getSeed() {
-  return await myStorage.get("memo").then((val) => {
-    return val.memo;
-  });
-}
+var timestamp = {
+  date: -1,
+};
 
 const random = (length = 10) => {
   return Math.random().toString(16).substr(2, length);
@@ -60,14 +58,16 @@ async function init() {
   myStorage.get("buttonOn").then(initTradeButton).catch(console.log);
   myStorage.get("rageOn").then(initRageButton).catch(console.log);
   myStorage.get("memo").then(initMemo).catch(console.log);
+  myStorage.get("timestamp").then(initTimestamp).catch(console.log);
 
   let hasDonated = await checkDonation();
-
-  console.log("Has donated: ", hasDonated);
 
   if (!hasDonated) {
     disableToggleButton();
     alertDonate();
+  } else {
+    enableToggleButton();
+    document.getElementById("alertDonate").style.visibility = "hidden";
   }
 }
 
@@ -78,17 +78,21 @@ function alertDonate() {
 function disableToggleButton() {
   setToggle(false);
   setRage(false);
-
   document.querySelector("#traderCheckBox").setAttribute("disabled", true);
   document.querySelector("#rageCheckBox").setAttribute("disabled", true);
 }
 
-// function enableToggleButton() {
-//   document.querySelector("#traderCheckBox").setAttribute("disabled", false);
-//   document.querySelector("#rageCheckBox").setAttribute("disabled", false);
-// }
+function enableToggleButton() {
+  document.querySelector("#traderCheckBox").setAttribute("enabled", true);
+  document.querySelector("#rageCheckBox").setAttribute("endabled", true);
+}
 
-//TODO Init text area for memo
+function initTimestamp(time) {
+  if (time.timestamp === undefined || time.timestamp.date === undefined) {
+    alertDonate();
+  }
+}
+
 function initMemo(mem) {
   if (mem.memo === undefined || mem.memo.seed === undefined) {
     setSeed(random());
@@ -151,8 +155,12 @@ function getActions(raw_actions, memo, till) {
     if (
       trace !== undefined &&
       trace.data !== null &&
-      trace.data.memo === memo.seed
+      new String(trace.data.memo)
+        .replace("\n", "")
+        .replace("\n", "")
+        .localeCompare(new String(memo.seed))
     ) {
+      setTime(new Date(actions[i].timestamp).getTime());
       return [false, false];
     }
   }
@@ -162,6 +170,24 @@ function getActions(raw_actions, memo, till) {
 async function setSeed(seed) {
   memo.seed = seed;
   myStorage.set({ memo });
+}
+
+async function getSeed() {
+  return await myStorage.get("memo").then((val) => {
+    return val.memo;
+  });
+}
+
+async function setTime(time) {
+  timestamp.date = time;
+  myStorage.set({ timestamp });
+}
+
+async function getTime() {
+  return await myStorage.get("timestamp").then((val) => {
+    if (val.timestamp) return val.timestamp.date;
+    return new Date().getTime();
+  });
 }
 
 async function setToggle(value) {
@@ -188,12 +214,30 @@ document.getElementById("rageToggle").onchange = function () {
   setRage(!isRage);
 };
 
-// document.getElementById("donateButton").onclick = function () {
-//   if (checkDonation) {
-//     enableToggleButton();
-//     createCookie();
-//   }
-// };
+// Update the count down every 1 second
+setInterval(async function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+  var week = 7 * 24 * 60 * 60 * 1000;
+  // Find the distance between now and the count down date
+  var date = await getTime();
+  var distance = week - (now - date);
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    // clearInterval(x);
+    document.getElementById("countdown").innerHTML = "";
+  }
+}, 1000);
 
 init().catch(console.log);
 
